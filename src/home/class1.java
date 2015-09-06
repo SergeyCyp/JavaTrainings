1a6b47c2268e6eb4bebb410906724913aa88c6b7
SergeyCyp <srg6201@mail.ru>
2015-09-06 19:45:00 +0300

    test

---
+++ src/home/class1.java
@@ -0,0 +1,12 @@
+package home;
+
+/**
+ * Created by User on 06.09.2015.
+ */
+public class class1 {
+
+    public static void main(String[] args) {
+        System.out.println("Helloy java");
+    }
+
+}
